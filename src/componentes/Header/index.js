09c2d1318fb7460abee48f88357f5936b86be356
DomBarbosa8 <mobarbosa.phoenix@gmail.com>
2021-11-07 16:24:56 -0300

    alteração no index-Header

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -8,8 +8,8 @@ const Header = ({empresa}) => {
             <nav>
                 <ul>
                     {/*eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
-                    <li><a href="https://pt.wikipedia.org/wiki/Z%C3%A9_Pelintra" target="_blank">história</a></li>
-                    <li><a href="https://www.youtube.com/watch?v=jZgZGzM6UCI" target="_blank">pontos</a></li>
+                    <li><a href="https://pt.wikipedia.org/wiki/Z%C3%A9_Pelintra" target="_blank" rel="noreferrer">história</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=jZgZGzM6UCI" target="_blank" rel="noreferrer">pontos</a></li>
                 </ul>
             </nav>
         </header>
